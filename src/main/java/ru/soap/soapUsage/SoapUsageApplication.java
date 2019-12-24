10d4bd7f9ca0b4e04b72afc48a7049e43e04e796
Yaroslav Lunev <jarik-stan@mail.ru>
2019-12-24 22:05:55 +0300

    Init

---
+++ src/main/java/ru/soap/soapUsage/SoapUsageApplication.java
@@ -0,0 +1,12 @@
+package ru.soap.soapUsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapUsageApplication.class, args);
+	}
+}
